b886c15d86a11c952dc09582466bc5a54f94d26e
Nick Bohl <ntbohl@gmail.com>
2018-11-06 10:27:09 -0500

    ammendments and file uploads

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -20,9 +20,7 @@
 fars_read <- function(filename) {
   if(!file.exists(filename))
     stop("file '", filename, "' does not exist")
-  data <- suppressMessages({
-    readr::read_csv(filename, progress = FALSE)
-  })
+  data <- readr::read_csv(filename, progress = FALSE)
   dplyr::tbl_df(data)
 }
 
